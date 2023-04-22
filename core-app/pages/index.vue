6bf0c6160c614b8346001732be6f08af1f5d4bd1
Pedro Lages <pedrolages@bravado.co>
2023-04-22 17:43:30 -0500

    first commit

---
+++ core-app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Core app</h1>
+    <iframe
+      id="inlineFrameExample"
+      title="Inline Frame Example"
+      width="300"
+      height="200"
+      src="http://localhost:5173"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
---
+++ war-room/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Hello from War-room</h1>
+</template>
